a36751a8546615f70352d2dd5b993859b7b76c7a
Zheng Yuxiang <yuxiang32@gmail.com>
2023-01-18 12:57:22 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# ELI5 anything about programming 
+### Welcome 👋
+This is a side project I built, incorporating GPT-3 and allowing newbie developers to ask anything about programming in a simplified format.
+
+I learnt alot about GPT-3 prompts and NextJS in this project. Really fun and interesting project for me! :)
+
+[ELI5 Programming](https://eli5programming.zhengyuxiang.repl.co)
---
+++ pages/api/generate.js
@@ -0,0 +1,31 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+`
+Write me a ELI5 explanation in the style of Paul Graham with the title below. Please make sure the blog post goes in-depth on the topic and shows that the writer did their research.
+
+Title:
+`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.8,
+    max_tokens: 1250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
